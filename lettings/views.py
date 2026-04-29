452a6f6915e396654c9801bc289e5bd3e4217020
SiRipo92 <ripochesierra@gmail.com>
2026-04-29 15:29:54 +0200

    Refactor: split monolith into lettings and profiles apps with data migrations

---
+++ lettings/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Letting
+
+def index(request):
+    """Display page for Lettings (lists view)"""
+    lettings_list = Letting.objects.all()
+    context = {'lettings_list': lettings_list}
+    return render(request, 'lettings/index.html', context)
+
+
+def letting(request, letting_id):
+    """Single page display for a Letting Item (detailed view)"""
+    letting = Letting.objects.get(id=letting_id)
+    context = {
+        'title': letting.title,
+        'address': letting.address,
+    }
+    return render(request, 'lettings/letting.html', context)
